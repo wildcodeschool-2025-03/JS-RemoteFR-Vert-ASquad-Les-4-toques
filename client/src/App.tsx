107a06e575705f3503131cb47084efc29466a1e9
NabilY-5 <nabilyahya59280@gmail.com>
2025-06-19 09:09:31 +0200

    fix: updated order

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Outlet } from "react-router";
-import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
