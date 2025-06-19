dbb870cc2cb18f7a19ee19cb05101cddb8e067f8
GARY GRAS <gary.gras07@gmail.com>
2025-06-19 14:31:15 +0200

    feat: creation of homepage and add carousel and carousel package

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,7 @@ import { RouterProvider, createBrowserRouter } from "react-router";
 
 // Import the main app component
 import App from "./App";
+import Homepage from "./pages/Homepage";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
@@ -21,7 +22,8 @@ import App from "./App";
 const router = createBrowserRouter([
   {
     path: "/", // The root path
-    element: <App />, // Renders the App component for the home page
+    element: <App />,
+    children: [{ path: "/", element: <Homepage /> }], // Renders the App component for the home page
   },
   // Try adding a new route! For example, "/about" with an About component
 ]);
