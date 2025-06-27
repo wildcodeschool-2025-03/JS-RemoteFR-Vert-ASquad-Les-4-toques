90e488cc6dde94e01d69f124eb1ded4c7cbeba5d
NabilY-5 <nabilyahya59280@gmail.com>
2025-06-27 11:49:34 +0200

    fix: updating schema.sql without insert

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,23 +1,3 @@
-CREATE TABLE category (
-  id INT AUTO_INCREMENT PRIMARY KEY ,
-  name VARCHAR(25)
-);
-
-CREATE TABLE recipe (
-id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-name VARCHAR(45),
-cost INT UNSIGNED NOT NULL,
-difficulty INT UNSIGNED NOT NULL,
-nb_people INT UNSIGNED NOT NULL,
-qte_ingredients INT UNSIGNED NOT NULL,
-picture TEXT,
-additional_text VARCHAR(255),
-is_validated BOOLEAN NOT NULL,
-category_id INT,
-FOREIGN KEY (category_id) REFERENCES category(id),
-user_id INT 
-);
-
 CREATE TABLE role (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   label VARCHAR(45)
