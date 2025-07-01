3c5498e9a3149a1cbcc514e47da1df4cb5139c85
anacslr <anais.cieslar@gmail.com>
2025-07-01 14:08:25 +0200

    feat: create browse & read for label

---
+++ server/src/modules/label/labelRepository.ts
@@ -0,0 +1,28 @@
+import databaseClient from "../../../database/client";
+
+import type { Rows } from "../../../database/client";
+
+type labelType = {
+  id: number;
+  label: string;
+  img: string;
+};
+
+class LabelRepository {
+  async readAll() {
+    const [rows] = await databaseClient.query<Rows>(
+      "SELECT id, label, img FROM label",
+    );
+    return rows as labelType[];
+  }
+
+  async read(id: number) {
+    const [rows] = await databaseClient.query<Rows>(
+      "SELECT id, label, img FROM label WHERE id=? ",
+      [id],
+    );
+    return rows[0] as labelType;
+  }
+}
+
+export default new LabelRepository();
